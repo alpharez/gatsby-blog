22eaf1c72c9e0991c27c93d2aee76a43169e71a6
Steve Clement <steve.clement@alpharez.com>
2024-03-22 10:07:17 -0400

    Added bio to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
+import Bio from "../components/bio"
 
 const AboutPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -11,6 +12,7 @@ const AboutPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <h1>About Me</h1>
+      <Bio />
       <p>I am interested in technology, security, AI, and automation.  I like taking on difficult challenges, and I can help solve technology problems.  Please reach out via social media or email if you have any questions or opportunities.</p>
       <a href={"/resume-steve-2024.pdf"}>resume pdf</a>
       <br />
