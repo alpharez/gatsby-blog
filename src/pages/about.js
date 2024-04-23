8bb20acacaef93b0d833b6814b39ee05f4dd881c
Steve Clement <steve.clement@alpharez.com>
2024-04-23 14:40:05 -0400

    resume-4

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,7 @@ const AboutPage = ({ data, location }) => {
       <h1>About Me</h1>
       <Bio />
       <p>I am interested in technology, security, AI, and automation.  I like taking on difficult challenges, and I can help solve technology problems.  Please reach out via social media or email if you have any questions or opportunities.</p>
-      <a href={"/resume-steve-2024.pdf"}>resume pdf</a>
+      <a href={"/resume-steve-2024-4.pdf"}>resume pdf</a>
       <br />
       <hr />
       <StaticImage
