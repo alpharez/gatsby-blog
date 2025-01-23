c6aa063d232474a79c6b34cbe7443dbb4338213e
alpharez <steve.clement@alpharez.com>
2025-01-23 12:16:40 -0500

    resume-2025

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,17 +14,9 @@ const AboutPage = ({ data, location }) => {
       <h1>About Me</h1>
       <Bio />
       <p>I am interested in technology, security, AI, and automation.  I like taking on difficult challenges, and I can help solve technology problems.  Please reach out via social media or email if you have any questions or opportunities.</p>
-      <a href={"/resume-steve-2024-4.pdf"}>resume pdf</a>
+      <a href={"/resume-steve-2025.pdf"}>resume pdf</a>
       <br />
       <hr />
-      <StaticImage
-        formats={["auto", "webp", "avif"]}
-        src="../images/mtb.png"
-        width={325}
-        height={325}
-        quality={95}
-        alt="Linkedin icon"
-      />
 
     </Layout>
   )
